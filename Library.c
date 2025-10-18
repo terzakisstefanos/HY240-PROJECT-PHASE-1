aa610e45b120d46f987656aa7db2551c68705594
Stefanos Terzakis <csd5873@csd.uoc.gr>
2025-10-18 16:20:30 +0300

    Refactor genre and book management functions

--- Library.c
+++ Library.c
@@ -26,6 +26,7 @@ genre_t * createGenre(int gid, char* name){//TODO you need to check the name <Ma
     }
     newgenre->book_sentinel.prev_in_genre=&newgenre->book_sentinel;
     newgenre->book_sentinel.next_in_genre=&newgenre->book_sentinel;
+    newgenre->book_sentinel.gid=gid;
     newgenre->display=NULL;
     newgenre->display_count=0;
     newgenre->gid=gid;
@@ -34,17 +35,110 @@ genre_t * createGenre(int gid, char* name){//TODO you need to check the name <Ma
     return newgenre;
 }
 
-genre_t *insertGenretolist(genre_t *head ,genre_t *node){
+void insertGenretolist(library_t *head ,genre_t *node){
     //TODO SEARCH IF THE GID IS ARLEADY HERE 
-    if(head->next_by_gid==NULL || node->gid < head->gid){// if list is empty or we need it to be the first node 
+    if(head->genres==NULL || node->gid < head->genres->gid){// if list is empty or we need it to be the first node 
         node->next_by_gid=head;
-        return node;
+        return ;
     }
     genre_t *current=head;
-    while (node->next_by_gid!=NULL && current->next_by_gid->gid < node->gid){//else find where it needs to go 
+    while (current->next_by_gid!=NULL && current->next_by_gid->gid < node->gid){//else find where it needs to go 
         current=current->next_by_gid;
     }
     node->next_by_gid=current->next_by_gid;
     current->next_by_gid=node;
-    return node;
+    return ;
+}
+// A function that creates a new book
+book_t *createBook(int bid , int gid,char* title){
+    book_t *newbook = malloc(sizeof(book_t));
+        if (newbook==NULL){
+        return NULL;
+    }
+    newbook->avg=0;
+    newbook->bid=bid;
+    newbook->gid=gid;
+    newbook->lost_flag=0;
+    newbook->next_global=NULL;
+    newbook->next_in_genre=NULL;
+    newbook->prev_in_genre=NULL;
+    newbook->sum_scores=0;
+    strcpy(newbook->title,title);
+    return newbook;
+}
+
+// A function that inserts a new book to the genre 
+void insertbooktogenre(genre_t * head,book_t *book){
+    //TODO check if book is unique with the search function 
+        if (head->book_sentinel.next_in_genre == &head->book_sentinel || book->avg > head->book_sentinel.next_in_genre->avg){
+        head->book_sentinel.next_in_genre=book;
+        head->book_sentinel.next_in_genre->prev_in_genre = book;
+        book->next_in_genre=&head->book_sentinel;
+        book->prev_in_genre=&head->book_sentinel;
+        if (head->book_sentinel.prev_in_genre == &head->book_sentinel) {
+            head->book_sentinel.prev_in_genre = book;
+        }
+        return;
+    }
+    book_t *current= head->book_sentinel.next_in_genre;
+    head->book_sentinel.avg=book->avg;
+    while (book->avg>current->avg){
+        current=current->next_in_genre;
+    }
+    if (current==&head->book_sentinel){// using the sentinel to make my algorith more efficient 
+        book->next_in_genre = &head->book_sentinel;
+        book->prev_in_genre = head->book_sentinel.prev_in_genre;
+        head->book_sentinel.prev_in_genre->next_in_genre = book;
+        head->book_sentinel.prev_in_genre = book;
+        return;
+    }
+    book->next_in_genre = current;
+    book->prev_in_genre = current->prev_in_genre;
+    current->prev_in_genre->next_in_genre = book;
+    current->prev_in_genre = book;
+    return;
+}
+// A function that adds books to the generic list of the library 200
+void insertbooktogeneric(library_t *head,book_t *book){
+    //TODO check if book is unique with the search function 
+    head->books = book;
+    if (head->books==NULL){
+        book->next_global=NULL;
+        return;
+    }
+    book->next_global=head->books;
+    return;
+}
+
+member_t *createmember(int sid, char* name){
+    member_t *newmember=malloc(sizeof(member_t));
+    if (newmember == NULL){
+        return NULL;
+    }
+    newmember->loans_head.sid = sid;    
+    newmember->loans_head.bid = -1;     
+    newmember->loans_head.next = NULL;
+    strcpy(newmember->name, name);
+    newmember->prev_all = NULL;
+    newmember->next_all = NULL;
+    newmember->sid = sid;
+    return newmember;
+}
+// A function that inserts a new member to the list 
+void insertmembertolist(library_t *head, member_t *newmember){
+        //TODO check for duplicates 
+    if (head->members == NULL || newmember->sid < head->members->sid) {
+        newmember->next_all = head->members;
+        newmember->prev_all = head;
+        if (head->members != NULL) {
+            head->members->prev_all= newmember;
+        }
+        head->members = newmember;
+        return;
+    }
+    member_t *current = head;
+    while(current->next_all!= NULL || current->sid< newmember->sid){
+        current=current->next_all;
+    }
+    // finish it 
 }
