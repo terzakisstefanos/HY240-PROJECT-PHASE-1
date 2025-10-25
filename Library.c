b74ca50fd336a84581dd7bd9ff3f824765e16a35
Stefanos Terzakis <csd5873@csd.uoc.gr>
2025-10-25 11:45:19 +0300

    added a lot also started on main

--- Library.c
+++ Library.c
@@ -4,14 +4,16 @@
 #include "Library.h"
 
 
-int main (void){
 
-}
+
+//**********************************CREATE FUNCTIONS**********************************
+
 // A function that creates the library
 library_t* createLibrary (void){
     library_t *library = malloc(sizeof(library_t));
     if (library == NULL) {
         return NULL;
+        printf("%s","IGNORED");
     }
     library->genres = NULL;
     library->members = NULL;
@@ -19,126 +21,244 @@ library_t* createLibrary (void){
     return library;
 }
 // A function that creates a new genre 
-genre_t * createGenre(int gid, char* name){//TODO you need to check the name <Maxname 
+genre_t * createGenre(int gid, char* name){
     genre_t *newgenre= malloc(sizeof(genre_t));
     if (newgenre==NULL){
+        printf("%s","IGNORED");
         return NULL;
     }
-    newgenre->book_sentinel.prev_in_genre=&newgenre->book_sentinel;
-    newgenre->book_sentinel.next_in_genre=&newgenre->book_sentinel;
-    newgenre->book_sentinel.gid=gid;
+    if (strlen(name)>=NAME_MAX){
+        printf("%s","WRONG NAME");
+        printf("%s","IGNORED");
+        return NULL;
+    }
+    newgenre->books=NULL;
     newgenre->display=NULL;
-    newgenre->display_count=0;
     newgenre->gid=gid;
-    strcpy(newgenre->name,name);
-    newgenre->next_by_gid=NULL;
+    newgenre->invalid_count=0;
+    newgenre->lost_count=0;
+    strcpy(newgenre->name,name);//TODO check for all of the creates where name is reqiured if it has the NULL terminator 
+    newgenre->next=NULL;
+    newgenre->slots=0;
     return newgenre;
 }
-
-void insertGenretolist(library_t *head ,genre_t *node){
-    //TODO SEARCH IF THE GID IS ARLEADY HERE 
-    if(head->genres==NULL || node->gid < head->genres->gid){// if list is empty or we need it to be the first node 
-        node->next_by_gid=head;
-        return ;
+// A function that creates a new member
+member_t *createmember(int sid, char* name){
+    member_t *newmember=malloc(sizeof(member_t));
+    if (newmember == NULL){
+        printf("%s","IGNORED");
+        return NULL;
     }
-    genre_t *current=head;
-    while (current->next_by_gid!=NULL && current->next_by_gid->gid < node->gid){//else find where it needs to go 
-        current=current->next_by_gid;
+    if (strlen(name)>=NAME_MAX){
+        printf("%s","WRONG NAME");
+        printf("%s","IGNORED");
+        return NULL;
     }
-    node->next_by_gid=current->next_by_gid;
-    current->next_by_gid=node;
-    return ;
+    newmember->loans=malloc(sizeof(loan_t));
+    if (newmember->loans==NULL){
+        free(newmember);
+        return NULL;
+    }
+    newmember->loans->bid=-1;
+    newmember->loans->next=newmember->loans;
+    newmember->loans->sid=-1;
+    strcpy(newmember->name, name);
+    newmember->next=NULL;
+    newmember->sid = sid;
+    return newmember;
 }
+
+
 // A function that creates a new book
 book_t *createBook(int bid , int gid,char* title){
     book_t *newbook = malloc(sizeof(book_t));
-        if (newbook==NULL){
+    if (newbook==NULL){
+        printf("%s","IGNORED");
+        return NULL;
+    }
+    if (strlen(title)>=TITLE_MAX){
+        printf("%s","WRONG NAME");
+        printf("%s","IGNORED");
         return NULL;
     }
     newbook->avg=0;
     newbook->bid=bid;
     newbook->gid=gid;
     newbook->lost_flag=0;
+    newbook->n_reviews=0;
+    newbook->next=NULL;
     newbook->next_global=NULL;
-    newbook->next_in_genre=NULL;
-    newbook->prev_in_genre=NULL;
+    newbook->prev=NULL;
     newbook->sum_scores=0;
     strcpy(newbook->title,title);
     return newbook;
 }
+//**********************************INSERT FUNCTIONS**********************************
+
+void insertGenretolist(library_t *head ,genre_t *node){
+    //TODO SEARCH IF THE GID IS ARLEADY HERE 
+    if(head->genres==NULL || node->gid < head->genres->gid){// if list is empty or we need it to be the first node 
+        node->next=head->genres;
+        head->genres = node;
+        printf("%s","DONE");
+        return;
+    }
+    genre_t *current=head->genres;
+    while (current->next!=NULL && current->next->gid < node->gid){//else find where it needs to go 
+        current=current->next;
+    
+    }
+    node->next=current->next;
+    current->next=node;
+    return;
+    printf("%s","DONE");
+}
 
 // A function that inserts a new book to the genre 
-void insertbooktogenre(genre_t * head,book_t *book){
-    //TODO check if book is unique with the search function 
-        if (head->book_sentinel.next_in_genre == &head->book_sentinel || book->avg > head->book_sentinel.next_in_genre->avg){
-        head->book_sentinel.next_in_genre=book;
-        head->book_sentinel.next_in_genre->prev_in_genre = book;
-        book->next_in_genre=&head->book_sentinel;
-        book->prev_in_genre=&head->book_sentinel;
-        if (head->book_sentinel.prev_in_genre == &head->book_sentinel) {
-            head->book_sentinel.prev_in_genre = book;
+void insertbooktogenre(genre_t *head, book_t *newbook) {
+    // TODO: check if the book is unique in this genre
+    if (head->books == NULL || newbook->avg > head->books->avg) {
+        newbook->next = head->books;
+        newbook->prev = NULL;
+        if (head->books != NULL) {
+            head->books->prev = newbook;
         }
+        head->books = newbook;
+        printf("%s","DONE");
         return;
     }
-    book_t *current= head->book_sentinel.next_in_genre;
-    head->book_sentinel.avg=book->avg;
-    while (book->avg>current->avg){
-        current=current->next_in_genre;
+    book_t *current = head->books;
+    while (current->next != NULL && current->next->avg > newbook->avg) {
+        current = current->next;
     }
-    if (current==&head->book_sentinel){// using the sentinel to make my algorith more efficient 
-        book->next_in_genre = &head->book_sentinel;
-        book->prev_in_genre = head->book_sentinel.prev_in_genre;
-        head->book_sentinel.prev_in_genre->next_in_genre = book;
-        head->book_sentinel.prev_in_genre = book;
-        return;
+    newbook->next = current->next;
+    newbook->prev = current;
+    if (current->next != NULL) {
+        current->next->prev = newbook;
     }
-    book->next_in_genre = current;
-    book->prev_in_genre = current->prev_in_genre;
-    current->prev_in_genre->next_in_genre = book;
-    current->prev_in_genre = book;
+    current->next = newbook;
+    printf("%s","DONE");
     return;
 }
+
 // A function that adds books to the generic list of the library 200
 void insertbooktogeneric(library_t *head,book_t *book){
     //TODO check if book is unique with the search function 
     head->books = book;
     if (head->books==NULL){
         book->next_global=NULL;
+         printf("%s","DONE");
         return;
     }
     book->next_global=head->books;
+    printf("%s","DONE");
     return;
 }
 
-member_t *createmember(int sid, char* name){
-    member_t *newmember=malloc(sizeof(member_t));
-    if (newmember == NULL){
-        return NULL;
-    }
-    newmember->loans_head.sid = sid;    
-    newmember->loans_head.bid = -1;     
-    newmember->loans_head.next = NULL;
-    strcpy(newmember->name, name);
-    newmember->prev_all = NULL;
-    newmember->next_all = NULL;
-    newmember->sid = sid;
-    return newmember;
-}
 // A function that inserts a new member to the list 
-void insertmembertolist(library_t *head, member_t *newmember){
-        //TODO check for duplicates 
+void insertmembertolist(library_t *head, member_t *newmember) {
+    // TODO: check for duplicates by sid
     if (head->members == NULL || newmember->sid < head->members->sid) {
-        newmember->next_all = head->members;
-        newmember->prev_all = head;
-        if (head->members != NULL) {
-            head->members->prev_all= newmember;
-        }
+        newmember->next = head->members;
         head->members = newmember;
+        printf("%s","DONE");
         return;
     }
-    member_t *current = head;
-    while(current->next_all!= NULL || current->sid< newmember->sid){
-        current=current->next_all;
+    member_t *current = head->members;
+    while (current->next != NULL && current->next->sid < newmember->sid) {
+        current = current->next;
     }
-    // finish it 
+    newmember->next = current->next;
+    current->next = newmember;
+    printf("%s","DONE");
+    return;
+}
+
+// A function that search the genrelist by gid 
+genre_t *searchgenre(library_t *head,int gid){
+    genre_t *current = head->genres;
+    while (current != NULL && current->gid != gid) {
+        current = current->next;
+    }
+    return current;
+}
+book_t *searchbook(genre_t *head, int bid){
+    book_t *current= head->books;
+    while (current != NULL && current->bid != bid){
+        current = current->next;
+    }
+    return current;
+}
+book_t *searchbooktogeneric(library_t *Lib, int bid){
+        book_t *current= Lib->books;
+    while (current != NULL && current->bid != bid){
+        current = current->next;
+    }
+    return current;
+}
+
+member_t *searchmember(library_t *Lib, int sid){
+    member_t *current= Lib->members;
+    while (current != NULL && current->sid != sid){
+        current = current->next;
+    }
+    return current;
+}
+loan_t *searchloan(member_t *head, int bid){
+    head->loans->bid=bid;
+    loan_t *current = head->loans->next;
+    while (current->bid != bid){
+        current = current->next;
+    }
+    if (current==head->loans){
+        return NULL;
+    }
+    return current;
+
+}
+// A function that add a loaned book to a member 
+void Loanbook(library_t *LIB, int sid, int bid){
+    member_t *member= searchmember(LIB,sid);
+    if (member==NULL){
+        printf("%s","IGNORED");
+        return;
+    }
+    loan_t *loaned=searchloan(member,bid);
+    if (loaned==NULL){
+        printf("%s", "IGNORED");
+        return;
+    }
+    book_t *bookingeneric = searchbooktogeneric(LIB,bid);
+    if (bookingeneric==NULL){
+        printf("%s","IGNORED");
+        return;
+    }
+    loan_t *newloan=malloc(sizeof(loan_t));
+        if (newloan==NULL){
+        printf("%s","IGNORED");
+        return;
+    }
+    newloan->sid=sid;
+    newloan->bid=bid;
+    newloan->next= member->loans->next;
+    member->loans->next=newloan;
+    printf("%s", "DONE");
+    return;
+}
+
+int main(void){
+    char filename[256];
+    printf ("%s","Give me the name of the file where the commands are: ");
+    if (fgets(filename,sizeof(filename),stdin)==NULL){
+        fprintf(stderr,"Error reading filename\n");
+    }
+    size_t length=strlen(filename);
+    if (length > 0 && filename[length-1] == '\n'){ 
+        filename[length-1] = '\0';
+    }
+    FILE * file=fopen(filename,"r");
+    if (file==NULL){
+        fprintf(stderr,"Error opening file\n");
+    }
+    
 }
--- Library2.h
+++ Library2.h
@@ -1,23 +1,20 @@
 /* =========================
    Κοινά & βοηθητικά
    ========================= */
-#ifndef LIBRARY_H
-#define LIBRARY_H
+#ifndef LIB_DS_H
+#define LIB_DS_H
 
 #include <stddef.h>
 
 #define TITLE_MAX 128
 #define NAME_MAX  64
 
-/*  Παγκόσμια θέση προβολών (ορίζεται από την εντολή S <slots>)
-    Φροντίστε να το κάνετε define στο αντίστοιχο .c αρχείο, ως
-    int SLOTS;
-*/
+/* Παγκόσμια θέση προβολών (ορίζεται από την εντολή S <slots>) */
 extern int SLOTS;
 
 /* -----------------------------------------
    LOAN: ενεργός δανεισμός (unsorted, O(1) insert/remove)
-   Λίστα ανά Member με χρήση sentinel node.
+   Λίστα ανά Member με sentinel head.
    ----------------------------------------- */
 typedef struct loan {
     int sid;            /* member id (ιδιοκτήτης της λίστας) */
@@ -38,67 +35,69 @@ typedef struct book {
 
     /* Στατιστικά δημοτικότητας */
     int sum_scores;                   /* άθροισμα έγκυρων βαθμολογιών */
-    int n_reviews;                       /* πλήθος έγκυρων βαθμολογιών */
+    int n_reviews;                    /* πλήθος έγκυρων βαθμολογιών */
     int avg;                          /* cache: floor(sum_scores / n_reviews); 0 αν n_reviews=0 */
     int lost_flag;                    /* 1 αν δηλωμένο lost, αλλιώς 0 */
 
-    /* Διπλά συνδεδεμένη λίστα του genre, ταξινομημένη κατά avg (desc). */
-    struct book *prev;
-    struct book *next;
+    /* Διπλά συνδεδεμένη λίστα του genre, ταξινομημένη κατά avg (desc).
+       — ΕΙΝΑΙ κυκλική με sentinel: δείκτες head <-> head στο genre. */
+    struct book *prev_in_genre;
+    struct book *next_in_genre;
 
     /* Προαιρετικό: συνδέσεις σε global ευρετήρια αν κρατήσετε (όχι απαραίτητο) */
-     struct book *next_global;         /* π.χ. unsorted λίστα όλων των βιβλίων */
+    struct book *next_global;         /* π.χ. unsorted λίστα όλων των βιβλίων */
 } book_t;
 
 /* -----------------------------------------
    MEMBER: μέλος βιβλιοθήκης
-   - Κρατά unsorted λίστα ενεργών δανεισμών (loan_t) με χρήση sentinel node
+   - Κρατά unsorted λίστα ενεργών δανεισμών (loan_t) με sentinel
    ----------------------------------------- */
 typedef struct member {
     int  sid;                         /* member id (μοναδικό) */
     char name[NAME_MAX];
 
     /* Λίστα ενεργών δανεισμών:
-       Uns. singly-linked με sentinel node:
+       Uns. singly-linked με sentinel (dummy head node):
        - Εισαγωγή: O(1) push-front
        - Διαγραφή γνωστού bid: O(1) αν κρατάτε prev pointer στη σάρωση */
-    loan_t* loans;
+    loan_t loans_head;                /* sentinel κόμβος: loans_head.next -> πρώτο loan ή NULL */
 
-    /* Μονοσυνδεδεμένη λίστα όλων των μελών ταξινομημένη κατά sid */
-    struct member *next;
+    /* Διπλά συνδεδεμένη λίστα όλων των μελών (προαιρετικό global index) */
+    struct member *prev_all;
+    struct member *next_all;
 } member_t;
 
-   /* -----------------------------------------
-      GENRE: κατηγορία βιβλίων
-      - Κρατά ΔΙΠΛΑ συνδεδεμένη λίστα ΒΙΒΛΙΩΝ ταξινομημένη κατά avg (desc)
-      - Κρατά και το αποτέλεσμα της τελευταίας D (display) για εκτύπωση PD
-      ----------------------------------------- */
-   typedef struct genre {
-      int  gid;                         /* genre id (μοναδικό) */
-      char name[NAME_MAX];
+/* -----------------------------------------
+   GENRE: κατηγορία βιβλίων
+   - Κρατά ΔΙΠΛΑ συνδεδεμένη λίστα ΒΙΒΛΙΩΝ ταξινομημένη κατά avg (desc)
+   - Η λίστα είναι ΚΥΚΛΙΚΗ με SENTINEL (book_sentinel)
+   - Κρατά και το αποτέλεσμα της τελευταίας D (display) για εκτύπωση PD
+   ----------------------------------------- */
+typedef struct genre {
+    int  gid;                         /* genre id (μοναδικό) */
+    char name[NAME_MAX];
 
-      /* διπλά συνδεδεμένη λίστα βιβλίων ταξινομημένη κατά avg φθίνουσα. */
-      book_t* books;
-      int lost_count;
-      int invalid_count;
+    /* Κυκλική διπλά συνδεδεμένη λίστα βιβλίων ταξινομημένη κατά avg φθίνουσα.
+       Sentinel: book_sentinel.prev_in_genre & next_in_genre δείχνουν στον εαυτό τους
+       όταν η λίστα είναι άδεια. */
+    book_t book_sentinel;
 
-      /* Αποτέλεσμα τελευταίας κατανομής D: επιλεγμένα βιβλία για προβολή.
-         Αποθηκεύουμε απλώς pointers στα book_t (δεν αντιγράφουμε δεδομένα). */
-      int slots;            /* πόσα επιλέχθηκαν για προβολή σε αυτό το genre */
-      book_t **display;     /* δυναμικός πίνακας με pointers στα επιλεγμένα βιβλία για προβολή */
+    /* Αποτέλεσμα τελευταίας κατανομής D: επιλεγμένα βιβλία για προβολή.
+       Αποθηκεύουμε απλώς pointers στα book_t (δεν αντιγράφουμε δεδομένα). */
+    int     display_count;            /* πόσα επιλέχθηκαν σε αυτό το genre */
+    book_t **display;                 /* δυναμικός πίνακας με pointers στα επιλεγμένα */
 
-      /* Μονοσυνδεδεμένη λίστα όλων των genres ταξινομημένη κατά gid (για εύκολη σάρωση). */
-      struct genre *next;
-   } genre_t;
+    /* Μονοσυνδεδεμένη λίστα όλων των genres ταξινομημένη κατά gid (για εύκολη σάρωση). */
+    struct genre *next_by_gid;
+} genre_t;
 
 /* -----------------------------------------
    LIBRARY: κεντρικός "ρίζας"
-   - Κρατά λίστα Genres (sorted by gid)
-   - Κρατά λίστα Members (sorted by sid)
+   - Κρατά λίστα Genres (sorted by gid) και προαιρετικά ευρετήρια
    ----------------------------------------- */
 typedef struct library {
     genre_t  *genres;     /* κεφαλή λίστας genres (sorted by gid) */
-    member_t *members;    /* διπλά συνδεδεμένη λίστα μελών (sorted by sid) */
+    member_t *members;    /* διπλά συνδεδεμένη λίστα μελών (sorted by sid) — προαιρετικό */
     book_t   *books;      /* unsorted λίστα όλων των books (ευκολία αναζήτησης) — προαιρετικό */
 } library_t;
 
@@ -106,11 +105,24 @@ typedef struct library {
    ΒΟΗΘΗΤΙΚΕΣ ΣΥΜΒΑΣΕΙΣ & INVARIANTS
    =========================
 
+   GENRE.book_sentinel:
+   - Είναι ΠΑΝΤΑ έγκυρος κόμβος-φρουρός (δεν αντιστοιχεί σε πραγματικό βιβλίο).
+   - Αρχικοποίηση:
+       g->book_sentinel.prev_in_genre = &g->book_sentinel;
+       g->book_sentinel.next_in_genre = &g->book_sentinel;
+       g->book_sentinel.gid = g->gid; // για ομοιομορφία
+   - Κενή λίστα: sentinel.next == sentinel.prev == &sentinel.
+
    ΕΙΣΑΓΩΓΗ βιβλίου σε genre (μετά από αλλαγή avg):
    - Επανατοποθέτηση με τοπικές αλλαγές δεικτών (O(hops)):
      1) Αφαιρείς το βιβλίο από την τωρινή θέση (splice-out).
      2) Σαρώνεις προς τα πάνω/κάτω μέχρι να βρεις τοπική θέση ως προς avg.
      3) Εισάγεις (splice-in) πριν από τον πρώτο κόμβο με avg < δικό μου.
+
+   MEMBER.loans_head:
+   - Είναι sentinel με sid=member.sid, bid=-1.
+   - Αρχικοποίηση: loans_head.next = NULL.
+   - Εισαγωγή: push-front (new->next = head.next; head.next = new).
 
    DISPLAY (αποτέλεσμα D):
    - Πριν τρέξει νέα D, απελευθερώνεις την προηγούμενη μνήμη (free genre->display),
@@ -122,7 +134,6 @@ typedef struct library {
    - Return book με έγκυρο score: ενημέρωση sum/n/avg (O(1)) + επανατοποθέτηση σε genre (O(hops))
    - Υπολογισμός D: μία σάρωση όλης της λίστας κάθε genre για points (O(#books_in_genre))
      + ταξινόμηση remainders (O(#genres log #genres))
-     + επιλογή κορυφαίων ανά genre (γραμμική στα seats(g)).
-*/
+     + επιλογή κορυφαίων ανά genre (γραμμική στα seats(g)).*/
 
-#endif
+#endif /* LIB_DS_H */
